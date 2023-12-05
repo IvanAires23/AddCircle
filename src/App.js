3d8303e87526455667a90269f766bd158d4b54b2
Ivan <ivanaires32@gmail.com>
2023-12-05 15:32:10 -0300

    feat: add circle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 function App() {
+  const [circles, setCircles] = useState([]);
+  const [redoCircles, setRedoCircles] = useState([]);
+
+  function handleAddCircle(e) {
+    console.log(e)
+    const { clientX, clientY } = e;
+    const newCircle = { x: clientX, y: clientY };
+    setCircles([...circles, newCircle]);
+    setRedoCircles([]);
+  };
+
+  console.log(circles)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container onClick={handleAddCircle}>
+      {circles.map((circle, index) => (
+        <Circle
+          left={circle.x}
+          top={circle.y}
+          key={index}
+        ></Circle>
+      ))}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 500px;
+  position: relative;
+`
+
+const Circle = styled.div`
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  left: ${props => (props.left - 21) + 'px'};
+  top: ${props => (props.top - 19) + 'px'};
+  background-color: red;
+`
 
 export default App;
