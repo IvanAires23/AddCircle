dc9b65c8147e4ebd0c9b35693112789d34df5dbb
Ivan <ivanaires32@gmail.com>
2023-12-05 16:10:18 -0300

    feat: undo circle

--- src/App.js
+++ src/App.js
@@ -6,24 +6,33 @@ function App() {
   const [redoCircles, setRedoCircles] = useState([]);
 
   function handleAddCircle(e) {
-    console.log(e)
     const { clientX, clientY } = e;
     const newCircle = { x: clientX, y: clientY };
     setCircles([...circles, newCircle]);
     setRedoCircles([]);
   };
 
-  console.log(circles)
+  function handleUndo() {
+    const lastCircle = circles[circles.length - 1]
+    const updateCircles = circles.slice(0, circles.length - 1)
+    setCircles(updateCircles)
+    setRedoCircles([...redoCircles, lastCircle])
+  }
 
   return (
-    <Container onClick={handleAddCircle}>
-      {circles.map((circle, index) => (
-        <Circle
-          left={circle.x}
-          top={circle.y}
-          key={index}
-        ></Circle>
-      ))}
+    <Container>
+      <Buttons>
+        <button onClick={handleUndo}>Desfazer</button>
+      </Buttons>
+      <Circles onClick={handleAddCircle}>
+        {circles.map((circle, index) => (
+          <Circle
+            left={circle.x}
+            top={circle.y}
+            key={index}
+          ></Circle>
+        ))}
+      </Circles>
     </Container>
   );
 }
@@ -34,14 +43,25 @@ const Container = styled.div`
   position: relative;
 `
 
+const Circles = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: beige;
+`
+
 const Circle = styled.div`
   position: absolute;
-  width: 30px;
-  height: 30px;
-  border-radius: 15px;
-  left: ${props => (props.left - 21) + 'px'};
-  top: ${props => (props.top - 19) + 'px'};
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  left: ${props => (props.left - 18) + 'px'};
+  top: ${props => (props.top - 17) + 'px'};
   background-color: red;
+`
+
+const Buttons = styled.div`
+  position: absolute;
+  z-index: 3;
 `
 
 export default App;
